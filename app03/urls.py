31389815e365de88d1446d90b68d2488602589f0
Pzyo <925975498@qq.com>
2021-08-10 23:52:28 +0800

    视图类

--- app03/urls.py
+++ app03/urls.py
@@ -5,4 +5,11 @@ from app03 import views
 urlpatterns = [
     url(r'^books/$', views.BooksView.as_view()),
     url(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view()),
+
+    # 使用ModelViewSet编写那5个接口
+    url(r'^books2/$', views.Books2View.as_view(actions={'get': 'list', 'post': 'create'})),  # 路径匹配上, 执行get方法, 对应Books2View.list
+    url(r'^books2/(?P<pk>\d+)$', views.Books2View.as_view(actions={'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+
+    # ViewSetMixin
+    url(r'^books3/$', views.Book3View.as_view(actions={'get': 'get_all_book'})),
 ]
--- app03/views.py
+++ app03/views.py
@@ -41,45 +41,101 @@ from rest_framework.response import Response
 #         return Response({'status': 100, 'msg': '删除成功'})
 
 # 基于GenericAPIView写的
-class BooksView(GenericAPIView):
-    # queryset要传queryset对象, 即查询的对象
-    # queryset = Book.objects.all()
-    queryset = Book.objects  # 解决源码发现, 如果传入Book.objects发现不是quertset对象, 会自动加 .all()
+# class BooksView(GenericAPIView):
+#     # queryset要传queryset对象, 即查询的对象
+#     # queryset = Book.objects.all()
+#     queryset = Book.objects  # 解决源码发现, 如果传入Book.objects发现不是quertset对象, 会自动加 .all()
+#
+#     # serializer_class指定使用哪个序列化类来序列化数据
+#     serializer_class = BookModelSerializer
+#
+#     def get(self, request):
+#         book_list = self.get_queryset()
+#         book_ser = self.get_serializer(book_list, many=True)
+#         return Response(book_ser.data)
+#
+#     def post(self, request):
+#         book_ser = self.get_serializer(data=request.data)
+#         if book_ser.is_valid():
+#             book_ser.save()
+#             return Response({'status': 200, 'msg': 'ok', 'data': book_ser.data})
+#         else:
+#             return Response({'status':101, 'msg':'校验失败'})
+#
+# class BookDetailView(GenericAPIView):
+#     queryset = Book.objects
+#     serializer_class = BookModelSerializer
+#
+#     def get(self, request, pk):
+#         book = self.get_object()
+#         book_ser = self.get_serializer(book)
+#         return Response(book_ser.data)
+#
+#     def put(self, request, pk):
+#         book = self.get_object()
+#         book_ser = self.get_serializer(instance=book, data=request.data)
+#         if book_ser.is_valid():
+#             book_ser.save()
+#             return Response({'status': 200, 'msg': 'ok', 'data': book_ser.data})
+#         else:
+#             return Response({'status':101, 'msg':'校验失败'})
+#
+#     def delete(self, request, pk):
+#         self.get_object().delete()
+#         return Response({'status': 100, 'msg': '删除成功'})
 
-    # serializer_class指定使用哪个序列化类来序列化数据
-    serializer_class = BookModelSerializer
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin
+# class BooksView(GenericAPIView, ListModelMixin, CreateModelMixin):
+#     # queryset要传queryset对象, 即查询的对象
+#     # queryset = Book.objects.all()
+#     queryset = Book.objects  # 解决源码发现, 如果传入Book.objects发现不是quertset对象, 会自动加 .all()
+#
+#     # serializer_class指定使用哪个序列化类来序列化数据
+#     serializer_class = BookModelSerializer
+#
+#     def get(self, request):
+#         return self.list(request)
+#
+#     def post(self, request):
+#         return self.create(request)
+#
+# class BookDetailView(GenericAPIView, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin):
+#     queryset = Book.objects
+#     serializer_class = BookModelSerializer
+#
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
+#
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+#
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)
 
-    def get(self, request):
-        book_list = self.get_queryset()
-        book_ser = self.get_serializer(book_list, many=True)
-        return Response(book_ser.data)
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
 
-    def post(self, request):
-        book_ser = self.get_serializer(data=request.data)
-        if book_ser.is_valid():
-            book_ser.save()
-            return Response({'status': 200, 'msg': 'ok', 'data': book_ser.data})
-        else:
-            return Response({'status':101, 'msg':'校验失败'})
-
-class BookDetailView(GenericAPIView):
+# class BooksView(ListAPIView, CreateAPIView):
+class BooksView(ListCreateAPIView):
     queryset = Book.objects
     serializer_class = BookModelSerializer
 
-    def get(self, request, pk):
-        book = self.get_object()
-        book_ser = self.get_serializer(book)
+
+# class BookDetailView(UpdateAPIView, RetrieveAPIView, DestroyAPIView):
+class BookDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects
+    serializer_class = BookModelSerializer
+
+
+from rest_framework.viewsets import ModelViewSet
+class Books2View(ModelViewSet):
+    queryset = Book.objects
+    serializer_class = BookModelSerializer
+
+
+from rest_framework.viewsets import ViewSetMixin
+class Book3View(ViewSetMixin, APIView):  # ViewSetMixin一定要放在APIView前面
+    def get_all_book(self, request):
+        book_list = Book.objects.all()
+        book_ser = BookModelSerializer(book_list, many=True)
         return Response(book_ser.data)
-
-    def put(self, request, pk):
-        book = self.get_object()
-        book_ser = self.get_serializer(instance=book, data=request.data)
-        if book_ser.is_valid():
-            book_ser.save()
-            return Response({'status': 200, 'msg': 'ok', 'data': book_ser.data})
-        else:
-            return Response({'status':101, 'msg':'校验失败'})
-
-    def delete(self, request, pk):
-        self.get_object().delete()
-        return Response({'status': 100, 'msg': '删除成功'})
