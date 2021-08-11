2e6d815ebb5ff1032a3388bc37e6f73d3a723710
欧阳志鹏 <925975498@qq.com>
2021-08-11 18:32:45 +0800

    路由

--- app03/urls.py
+++ app03/urls.py
@@ -2,6 +2,27 @@ from django.conf.urls import url
 
 from app03 import views
 
+# 第一步, 导入routers模块
+from rest_framework import routers
+
+# 第二步, 两个类, 实例化得到对象
+# routers.DefaultRouter
+# routers.SimpleRouter
+# router_obj = routers.SimpleRouter()
+router_obj = routers.DefaultRouter()
+# 第三步, 注册
+# 基本格式: router_obj.register('前缀', '继承自ModelViewSet的视图类', '别名') # ps: 前缀无需加斜杠
+router_obj.register(r'books4', views.Books4View)
+# 第四步
+# router_obj.urls自动生成的路由
+print(router_obj.urls)
+"""
+[
+    <RegexURLPattern book-list ^books4/$>, 
+    <RegexURLPattern book-detail ^books4/(?P<pk>[^/.]+)/$>
+]
+"""
+
 urlpatterns = [
     url(r'^books/$', views.BooksView.as_view()),
     url(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view()),
@@ -13,5 +34,8 @@ urlpatterns = [
     # ViewSetMixin
     url(r'^books3/$', views.Book3View.as_view(actions={'get': 'get_all_book'})),
 
-    url(r'^books4/$', views.Books4View.as_view(actions={'get': 'list'})),
+    # url(r'^books4/$', views.Books4View.as_view(actions={'get': 'list'})),
 ]
+
+# 第五步, 加入urlpatterns
+urlpatterns += router_obj.urls
